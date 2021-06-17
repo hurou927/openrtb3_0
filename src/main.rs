94d1c5f5852f4fe527e1878e3d59eb8e16f945b2
hurou927 <god.be.with.ye.fs@gmail.com>
2021-06-17 23:06:51 +0900

    [wip] base impl

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,44 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+// common
+struct BidRequest {
+    body: String,
+}
+struct BidResponse {
+    body: String,
+}
+
+struct SspZebra {}
+
+trait SspApi<HttpReq, HttpRes> {
+    fn parse(body: HttpReq) -> BidRequest;
+    fn format(res: BidResponse) -> HttpRes;
+}
+
+impl SspApi<String, String> for SspZebra {
+    fn parse(body: String) -> BidRequest {
+        BidRequest { body }
+    }
+    fn format(res: BidResponse) -> String {
+        res.body
+    }
+}
+
+struct SspController {}
+
+impl SspController {
+    fn process(req: BidRequest) -> BidResponse {
+        BidResponse { body: req.body }
+    }
+}
+
 #[post("/rtb/zebra")]
 async fn rtb_zebra(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+    let req = SspZebra::parse(req_body);
+    let res = SspController::process(req);
+    let body = SspZebra::format(res);
+    HttpResponse::Ok().body(body)
 }
 
 async fn metrics() -> impl Responder {
