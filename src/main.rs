ee7021bc1411d1b9e7c710d4587e40d572f846e7
hurou927 <god.be.with.ye.fs@gmail.com>
2021-06-17 21:40:09 +0900

    add test endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[post("/rtb/zebra")]
+async fn rtb_zebra(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn metrics() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    let host = format!("127.0.0.1:{}", port);
+
+    println!("start: {}", host);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(echo)
+            .service(rtb_zebra)
+            .route("/metrics", web::get().to(metrics))
+    })
+    .bind(host)?
+    .run()
+    .await
 }
