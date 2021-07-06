cec276c747f896ed2ea44b683a51d16d3be970d8
hurou927 <god.be.with.ye.fs@gmail.com>
2021-07-06 21:55:44 +0900

    publisher

--- src/adcom/publisher.rs
+++ src/adcom/publisher.rs
@@ -1,11 +1,16 @@
-
 use serde::{Deserialize, Serialize};
+
+use super::category_taxonomy;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Publisher {
-  ext: Option<PublisherExt>,
+    id: Option<String>,
+    name: Option<String>,
+    domain: Option<String>,
+    cat: Vec<String>,
+    cattax: Option<category_taxonomy::CategoryTaxonomy>,
+    ext: Option<PublisherExt>,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct PublisherExt {}
-
